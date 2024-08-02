333597d41127402ae98a7397512f73f177cc2ea9
Soheil <itsupport@londonist.co.uk>
2024-08-02 15:15:01 +0300

    Update start command response message
    Changed the greeting message in `bot.ts` to a more enthusiastic "Welcome to my Deno bot yaaaaaaaaay!" This improves user engagement and sets a friendly tone for the bot interaction.

--- bot.ts
+++ bot.ts
@@ -2,7 +2,7 @@ import { Bot } from "https://deno.land/x/grammy@v1.28.0/mod.ts";
 
 const bot = new Bot("7277453739:AAGbYxEEWniDVcvVSmBYd21G-McQv5q95Fs");
 
-bot.command("start", (ctx) => ctx.reply("Welcome to my Deno bot!"));
+bot.command("start", (ctx) => ctx.reply("Welcome to my Deno bot yaaaaaaaaay!"));
 
 bot.on("message", (ctx) => ctx.reply("You said: " + ctx.message.text));
 
